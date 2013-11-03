1e874e9df04034a1d0648bbf4fb10b9d913bef07
r02548 <bruger@fullsail.edu>
2013-11-03 12:07:35 -0500

    Added files for project 2.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* Brandon Ruger
+ * ASD 1311
+ * Project 1 */
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#addalbum').on('pageinit', function(){
+	//code needed for add album page goes here
+});
+
+$('#saveditems').on('pageinit', function(){
+	//code needed for saved items page goes here
+});
