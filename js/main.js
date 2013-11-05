6ae1716a7f430ad6f75fbef19f9af10bacefbf68
r02548 <bruger@fullsail.edu>
2013-11-04 19:24:32 -0500

    Added line of code to append sublist to newLinksLi.

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ $('#saveditems').on('pageinit', function(){
             for (var n in findObject) {
                 var makeNewSubList = $('<li></li>').appendTo(subList)
                 var subText = findObject[n][0]+ " " +findObject[n][1];
-                makeNewSubList.html(subText);
+                makeNewSubList.html(subText).append(newLinksLi);
             }
             createEditDelLinks(localStorage.key(i), newLinksLi); //Create our edit and delete links for each item in local storage.
 
@@ -234,64 +234,64 @@ $('#saveditems').on('pageinit', function(){
         }
     }
     
-    function validateInput(eventData) {
-        //Define the elements we want to check
-        var getPetName = getElements('petname');
-        var getPetAge = getElements('petage');
-        var getPetType = getElements('pettype');
-        
-        //Reset Error Messages
-        error.innerHTML = "";
-        getPetName.style.border = "1px solid black";
-        getPetAge.style.border = "1px solid black";
-        getPetType.style.border = "1px solid black";
-        
-        
-        //Get Error Messages
-        var errorMessagesArray = [];
-        
-        //Pet Name Validation
-        if (getPetName.value === "") {
-            var nameError = "Please enter your pet's name.";
-            getPetName.style.border = "2px solid red";
-            errorMessagesArray.push(nameError);
-        }
-        
-        
-        //Pet Age Validation
-        var re = /^\d{1,2}$/;
-        if (!(re.exec(getPetAge.value))){
-            var ageError = "Please enter your pet's numerical age.";
-            getPetAge.style.border = "2px solid red";
-            errorMessagesArray.push(ageError);
-        }
-        
-        
-        //Pet Type Validation
-        if (getPetType.value === ""){
-            var typeError = "Please enter your pet's type.";
-            getPetType.style.border = "2px solid red";
-            errorMessagesArray.push(typeError);
-        }
-        
-        //If there were errors, display them on screen.
-        if (errorMessagesArray.length >= 1) {
-            for (var i=0, j=errorMessagesArray.length; i<j; i++) {
-                var txt = document.createElement('li');
-                txt.innerHTML = errorMessagesArray[i];
-                error.appendChild(txt);
-;            }
-
-            eventData.preventDefault();
-            return false;
-        } else{
-            //If all is OK, save our data!
-            //Send key value which came from edit function.
-            //Remember this key value was passed through the event Listener as a property.
-            submitData(this.key);
-        }
-
-    }
+//    function validateInput(eventData) {
+//        //Define the elements we want to check
+//        var getPetName = getElements('petname');
+//        var getPetAge = getElements('petage');
+//        var getPetType = getElements('pettype');
+//        
+//        //Reset Error Messages
+//        error.innerHTML = "";
+//        getPetName.style.border = "1px solid black";
+//        getPetAge.style.border = "1px solid black";
+//        getPetType.style.border = "1px solid black";
+//        
+//        
+//        //Get Error Messages
+//        var errorMessagesArray = [];
+//        
+//        //Pet Name Validation
+//        if (getPetName.value === "") {
+//            var nameError = "Please enter your pet's name.";
+//            getPetName.style.border = "2px solid red";
+//            errorMessagesArray.push(nameError);
+//        }
+//        
+//        
+//        //Pet Age Validation
+//        var re = /^\d{1,2}$/;
+//        if (!(re.exec(getPetAge.value))){
+//            var ageError = "Please enter your pet's numerical age.";
+//            getPetAge.style.border = "2px solid red";
+//            errorMessagesArray.push(ageError);
+//        }
+//        
+//        
+//        //Pet Type Validation
+//        if (getPetType.value === ""){
+//            var typeError = "Please enter your pet's type.";
+//            getPetType.style.border = "2px solid red";
+//            errorMessagesArray.push(typeError);
+//        }
+//        
+//        //If there were errors, display them on screen.
+//        if (errorMessagesArray.length >= 1) {
+//            for (var i=0, j=errorMessagesArray.length; i<j; i++) {
+//                var txt = document.createElement('li');
+//                txt.innerHTML = errorMessagesArray[i];
+//                error.appendChild(txt);
+//;            }
+//
+//            eventData.preventDefault();
+//            return false;
+//        } else{
+//            //If all is OK, save our data!
+//            //Send key value which came from edit function.
+//            //Remember this key value was passed through the event Listener as a property.
+//            submitData(this.key);
+//        }
+//
+//    }
     
     //Variable Defaults
     var fleaMedication = ["--Type of Flea Medication--", "Topical", "Oral", "Sprayon"];
