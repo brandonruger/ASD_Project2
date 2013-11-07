51ae60394b662df042d358f177074752d2828077
r02548 <bruger@fullsail.edu>
2013-11-07 17:53:26 -0500

    Revert 199d7b0..f8ef0d0
    This rolls back to commit 199d7b0ee4436a021382abd99482c3c042906360.

--- js/main.js
+++ js/main.js
@@ -10,28 +10,6 @@ $('#addalbum').on('pageinit', function(){
     
     //code needed for add album page goes here
     
-    //Function to hide form
-        
-    //function toHideForm(n) {
-    //    switch (n){
-    //        case "on":
-    //            $("#addAlbumForm").css("display", "none");
-    //            $("#clearData").css("display", "inline");
-    //            $("#displayData").css("display", "none");
-    //            $("#addNewReminder").css("display", "inline");
-    //            break;
-    //        case "off":
-    //            $("#addAlbumForm").css("display", "block");
-    //            $("#clearData").css("display", "inline");
-    //            $("#displayData").css("display", "inline");
-    //            $("#addNewReminder").css("display", "none");
-    //            $("#items").css("display", "none");
-    //            break;
-    //        default:
-    //            return false;
-    //    }
-    //}
-    
     //Create function to submit data.
     function submitData(key) {
         //If there is no key, this means this is a brand new item and we need a new key.
@@ -57,38 +35,41 @@ $('#addalbum').on('pageinit', function(){
             alert("Album has been added!");
     }
     
-
+    //Set Link & Submit Click Events
     
-    //Get Data from Local Storage
-    function getDataFromStorage() {
-        //toHideForm("on");
-        if (localStorage.length === 0) {
-            alert("There is no data in Local Storage so default data was added.");
-            getJsonData();
-        }
-        
-        //jQuery code to write data from local storage to the browser
-        $('<div id="items"><ul></ul></div>').appendTo("#addalbum").css("display", "block");
-        for (var i=0; i<localStorage.length; i++){
-            var newListItem = $('<li></li>').appendTo("#items > ul");
-            var key = localStorage.key(i);
-            var dataValue = localStorage.getItem(key);
-            //Convert string from local storage back to an object.
-            var findObject = JSON.parse(dataValue);
-            var subList = $('<ul></ul>').appendTo(newListItem);
-            for (var n in findObject) {
-                var makeNewSubList = $('<li></li>').appendTo(subList)
-                var subText = findObject[n][0]+ " " +findObject[n][1];
-                makeNewSubList.html(subText);
-            }
-            createEditDelLinks(localStorage.key(i), newListItem); //Create our edit and delete links for each item in local storage.
+    
+    //var displayXmlData = $("#displayXml");
+    //displayXmlData.on("click", getXmlDataFromStorage);
 
-        }
+    var saveData = $("#saveAlbumButton");
+    saveData.on("click", submitData);
+    //var createButton = $("#button");
+    //createButton.on("click", validateInput);
+        
+});
+
+$('#jsonpage').on('pageinit', function(){
+    //code needed for home page goes here
+    
+    //Retrieve JSON data from Ajax
+    
+    function getJsonDataFromStorage(){
+        console.log("Ajax function is attempting to run");    
+        $("#jsoncontent").empty();
+        $.ajax({
+            url: 'xhr/json.js',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data, status){
+                console.log("data - " + data, status);
+            }
+        })
     }
     
-        //Dynamically create Edit & Delete Links
+    
+    //Dynamically create Edit & Delete Links
     function createEditDelLinks(key, newListItem) {
-        var editLink = $('<ul><li><a href="#">Edit Item</a></li></ul>').appendTo("#addAlbumForm").on("click", editReminder);
+        var editLink = $('<ul><li><a href="#">Edit Item</a></li></ul>').appendTo("#jsoncontent").on("click", editReminder);
         editLink.key = key;
         $(editLink).append(newListItem);
         console.log(editLink.key);
@@ -97,19 +78,12 @@ $('#addalbum').on('pageinit', function(){
         var breakTag = $('br').appendTo("#addAlbumForm");
         
         //add delete single item link
-        var deleteLink = $('<ul><li><a href="#">Delete Item</a></li></ul>').appendTo("#addAlbumForm").on("click", deleteReminder);
+        var deleteLink = $('<ul><li><a href="#">Delete Item</a></li></ul>').appendTo("#jsoncontent").on("click", deleteReminder);
         deleteLink.key = key;
         $(deleteLink).append(newListItem);
         
-    }
-    
-        //Auto populate Local Storage with JSON data
-    function getJsonData() {
-        //Store JSON Object into Local Storage.
-        for (var n in json) {
-            var id = Math.floor(Math.random()*10000001);
-            localStorage.setItem(id, JSON.stringify(json[n]));
-        }
+        return key;
+        
     }
     
     function editReminder() {
@@ -119,24 +93,12 @@ $('#addalbum').on('pageinit', function(){
         console.log(lsData);
         var itemList = JSON.parse(lsData);
         console.log(itemList);
-        
-        //Show the form
-        //toHideForm("off");
+
         
         //Populate form fields with current localStorage values.
         $("#artist").val(itemList.artist[1]);
         $("#album").val(itemList.album[1]);
-        $("#format").val(itemList.format[1]);
-
-        //if(itemList.radio1[1] == "current-collection") {
-        //    $("#radio1").attr("checked", "checked");
-        //}
-        //        
-        //if (itemList.radio2[1] == "wish-list") {
-        //    $("#radio2").attr("checked", "checked");
-        //    
-        //}
-                
+        $("#format").val(itemList.format[1]);                
         $("#date").val(itemList.date[1]);
         $("#notes").val(itemList.notes[1]);
         
@@ -174,109 +136,14 @@ $('#addalbum').on('pageinit', function(){
             return false;
         }
     }
+
     
-    //Variable Defaults
-    var radio1Val;
-    var radio2Val;
-    
-    //Set Link & Submit Click Events
-    
-    var displayJsonData = $("#displayJson");
-    displayJsonData.on("click", getDataFromStorage);
-    var displayXmlData = $("#displayXml");
-    displayXmlData.on("click", getDataFromStorage);
+    var displayJsonData = $("#jsonbutton");
+    displayJsonData.on("click", getJsonDataFromStorage);
     var clearData = $("#clearData");
     clearData.on("click", clearLocalStorage);
-    var saveData = $("#saveAlbumButton");
-    saveData.on("click", submitData);
-    //var createButton = $("#button");
-    //createButton.on("click", validateInput);
-        
 });
 
-/* Code to refactor */
-
-    
-    
-    
-
-    
-    
-    
-    
-    
-        
-        //Write Data from local storage to the browser
-        //var createDiv = document.createElement('div');
-        //createDiv.setAttribute("id", "items");
-        //var createList = document.createElement('ul');
-        //createDiv.appendChild(createList);
-        //document.body.appendChild(createDiv);
-        //$("#items").css("display", "block");
-        //for (var i=0; i<localStorage.length; i++) {
-        //    var createListItem = document.createElement('li');
-        //    var newListItem = document.createElement('li');
-        //    createList.appendChild(createListItem);
-            //var key = localStorage.key(i);
-            //var dataValue = localStorage.getItem(key);
-            ////Convert string from local storage back to an Object.
-            //var findObject = JSON.parse(dataValue);
-            //var subList = document.createElement('ul');
-            //createListItem.appendChild(subList);
-            //getIcon(findObject.fleaRx[1], subList);
-            //for (var n in findObject) {
-            //    var makeSublist = document.createElement('li');
-            //    subList.appendChild(makeSublist);
-                //var subText = findObject[n][0]+ " " +findObject[n][1];
-                //makeSublist.innerHTML = subText;
-    //            createListItem.appendChild(newListItem);
-    //        }
-    //        createEditDelLinks(localStorage.key(i), newListItem); //Create our edit and delete links for each item in local storage.
-    //        
-    //    }
-    //    
-    //}
-    
-        
-    
-    
-
-    //Function to create edit/delete links for each stored item when displayed.
-    //function createEditDelLinks(key, newListItem) {
-    //    //add edit single item link
-    //    var editLink = document.createElement('a');
-    //    editLink.href = "#";
-    //    editLink.key = key;
-    //    var editText = "Edit Reminder";
-    //    editLink.on("click", editReminder);
-    //    editLink.innerHTML = editText;
-    //    newListItem.appendChild(editLink);
-    
-        //    //add line break
-        //var breakTag = document.createElement('br');
-        //newListItem.appendChild(breakTag);
-        
-        // //add delete single item link
-        //var deleteLink = document.createElement('a');
-        //deleteLink.href = "#";
-        //deleteLink.key = key;
-        //var deleteText = "Delete Reminder";
-        //deleteLink.on("click", deleteReminder);
-        //deleteLink.innerHTML = deleteText;
-        //newListItem.appendChild(deleteLink);
-
-        
-        
-    
-
-    
-
-    
-
-    
-
-    
-    
-    
-    
-    
+$('#xmlpage').on('pageinit', function(){
+    //code needed for home page goes here
+});
